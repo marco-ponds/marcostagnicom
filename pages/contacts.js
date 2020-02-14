1eb8bde98041693d206030b5ca7ccf8d9a61ba76
Marco Stagni <m.stagni@elsevier.com>
2020-02-14 12:51:58 +0000

    fixed responsiveness

--- pages/contacts.js
+++ pages/contacts.js
@@ -7,6 +7,7 @@ import "antd/dist/antd.min.css";
 
 export default () => (
     <React.Fragment>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <GoogleAnalytics />
         <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.39.1/mapbox-gl.css' rel='stylesheet' />
         <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer/>
--- pages/projects.js
+++ pages/projects.js
@@ -7,6 +7,7 @@ import "antd/dist/antd.min.css";
 
 export default () => (
     <React.Fragment>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <GoogleAnalytics />
         <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.39.1/mapbox-gl.css' rel='stylesheet' />
         <Layout currentPage='projects'>
