9b06713f8380dc91d7509a25d8d2c63a02fa3fb1
Marco Stagni <m.stagni@elsevier.com>
2020-02-14 13:47:47 +0000

    responsiveness improved

--- app/main/Whoami.js
+++ app/main/Whoami.js
@@ -5,7 +5,7 @@ const Whoami = () => (
         <h1>Hi, I'm Marco</h1>
         <h3>and I create cool things.</h3>
         <p className='description'>
-                I'm a <span className='jobtitle'>Software Engineer</span> base in London, Uk. I'm a tech enthusiast who builds interactive
+                I'm a <span className='jobtitle'>Software Engineer</span> based in London, Uk. I'm a tech enthusiast who builds interactive
                 web applications for millions of users. I'm driven by curiosity, attention for details and passion.
         </p>
     </div>
