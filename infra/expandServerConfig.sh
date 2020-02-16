f01ddfe384b78579c20d300b91b50ee24cb7142d
Marco Stagni <mrc.stagni@gmail.com>
2020-02-16 16:31:52 +0000

    testing env vars 4

--- infra/expandServerConfig.sh
+++ infra/expandServerConfig.sh
@@ -2,11 +2,9 @@
 
 echo 'saving env variables'
 
-DEST_FILE = 'server/config.json'
-
 JSON_STRING='{
     "TELEGRAM_TOKEN": "'"$TELEGRAM_TOKEN"'",
     "TELEGRAM_CHAT_ID": "'"$TELEGRAM_CHAT_ID"'"
 }'
 
-echo $JSON_STRING > $DEST_FILE
+echo $JSON_STRING > server/config.json
